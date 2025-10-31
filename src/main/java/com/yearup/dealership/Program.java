701f70d46abc65ef811d7c26bb6cb77edc8b67b5
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-10-31 13:03:26 -0400

    menu added

--- src/main/java/com/yearup/dealership/Program.java
+++ src/main/java/com/yearup/dealership/Program.java
@@ -4,7 +4,7 @@ public class Program {
     public static void main(String[] args) {
 
        UserInterface userInterface = new UserInterface();
-       userInterface.display();
+       userInterface.displayMenu();
 
     }
 }
